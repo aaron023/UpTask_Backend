30cd25008ee54a3708acad8bde99dc5f01276a83
Aaron Carrizales <aaronacr@gmail.com>
2024-10-07 13:23:59 -0600

    se corrige fix en el modelo Token.ts

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -15,7 +15,7 @@ const tokenSchema : Schema = new Schema({
         type: Types.ObjectId,
         ref: 'User'
     },
-    expiresAt: {
+    createAt: {
         type: Date,
         default: () => Date.now(),
         expires: "10m"
